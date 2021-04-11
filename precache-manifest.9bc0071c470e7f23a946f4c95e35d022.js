c25c82f38c9dda698d0dba8cdcb1db11b62351aa
Code Sandbox <hello@codesandbox.io>
2021-04-11 09:58:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9bc0071c470e7f23a946f4c95e35d022.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2359f6873d5f18b90764ae5cdb9fecf1",
+    "url": "https://szoftverfejleszto2016.github.io/csb-xglc5/index.html"
+  },
+  {
+    "revision": "c0b5798c1ed06bcd1e3a",
+    "url": "https://szoftverfejleszto2016.github.io/csb-xglc5/static/js/2.1ba3f0d6.chunk.js"
+  },
+  {
+    "revision": "0627c79309484ef68e08",
+    "url": "https://szoftverfejleszto2016.github.io/csb-xglc5/static/js/main.4acb7c83.chunk.js"
+  },
+  {
+    "revision": "bfcb25fde72cfe18b9a2",
+    "url": "https://szoftverfejleszto2016.github.io/csb-xglc5/static/js/runtime-main.8329396c.js"
+  }
+]);
